81f1ce1cc652ccb245293f1e681fd2d8548fb341
Rafa G. Miranda ℝ - 𝟙𝟚 <89557826+RafaGdeom@users.noreply.github.com>
2026-03-08 12:04:49 -0300

    Add files via upload

---
+++ 0.02/Program.cs
@@ -0,0 +1,16 @@
+﻿//0.02 - A partir da digitação do ano de nascimento de um aluno, calcule a idade e use a expressão 
+//condicional para apresentar se o aluno é maior de idade ou menor
+
+Console.WriteLine("\nDigite o ano de nascimento do aluno: \n");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+int nasc = 2026 - idade;
+
+if(nasc >=18)
+{
+    Console.WriteLine($"\nIdade de {nasc} anos Maior de idade.");
+}
+else
+{
+    Console.WriteLine($"\nIdade de {nasc} anos De menor!!!");
+}
