39f32bc980cca9b2cf82ed3a653282bf7940aed0
Rafa G. Miranda ℝ - 𝟙𝟚 <89557826+RafaGdeom@users.noreply.github.com>
2026-03-09 13:50:57 -0300

    Add files via upload

---
+++ 0.03/Program.cs
@@ -0,0 +1,15 @@
+﻿//0.03 - Implemente a tabuda do 7.
+Console.WriteLine("\n=== TABUADA DO 7===\n");
+Console.WriteLine("CONDIÇÃO FOR\n");
+for(int i = 1; i <= 10 ; i++)
+{
+    Console.WriteLine($"{i} x 7 = {i * 7}");
+}
+Console.WriteLine();
+Console.WriteLine("CONDIÇÃO WHILE\n");
+int y = 1;
+while(y <= 10)
+{
+    Console.WriteLine($"{y} x 7 = {y * 7}");
+    y += 1;
+}
