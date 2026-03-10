37acf0b8220f3a3a539fb3615b680f3d2725b50b
Rafa G. Miranda ℝ - 𝟙𝟚 <89557826+RafaGdeom@users.noreply.github.com>
2026-03-10 07:23:26 -0300

    Add files via upload

---
+++ 0.04/Program.cs
@@ -0,0 +1,13 @@
+﻿// 0.04 - Criar um algoritmo para somar as idades de 5 alunos. Apresente o resultado.
+int idade = 0;
+int soma = 0;
+
+for(int i = 0; i <= 5; i++)
+{
+    Console.Write($"Digite a idade do aluno {i}: ");
+    idade = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine();
+    soma += idade;
+}
+int media = soma / 5;
+Console.WriteLine($"A media das idades será: {media} anos.");
