508635becbe39469560ca90712a12e3fab82eb11
Rafa G. Miranda ℝ - 𝟙𝟚 <89557826+RafaGdeom@users.noreply.github.com>
2026-03-11 13:53:15 -0300

    Add files via upload

---
+++ 0.05/Program.cs
@@ -0,0 +1,11 @@
+﻿//0.05 - Criar o programa para calcular e apresentar a tabuada do 5 ao 10.
+//Utilize duas estruturas de repetição.
+for(int i = 5; i <= 10; i++)
+{
+    Console.WriteLine($"===Taboada do {i} ===");
+    for(int y = 1; y <= 10; y++)
+    {
+        Console.WriteLine($"{y} x {i} = {y * i}");
+    }
+    System.Console.WriteLine();
+}
