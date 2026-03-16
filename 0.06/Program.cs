cb38bbc833f55aa2cdb7fc8e7c96bc00c0ea7e3e
Rafa G. Miranda ℝ - 𝟙𝟚 <89557826+RafaGdeom@users.noreply.github.com>
2026-03-16 00:03:43 -0300

    Add files via upload

---
+++ 0.06/Program.cs
@@ -0,0 +1,13 @@
+﻿//0.06 - Faça um programa que receba três notas, calcule e mostre a média aritmética.
+double nota = 0;
+double somaNota = 0;
+double media = 0;
+for(int i = 0; i < 3; i++)
+{
+    Console.Write($"Digite a nota {i+1}: ");
+    nota = Convert.ToDouble(Console.ReadLine());
+    somaNota += nota;
+}
+media = somaNota / 3;
+Console.WriteLine($"A média aritimética das notas são {media}");
+Console.WriteLine(somaNota);
