12f39ee44e93c5434aba864658c7dd4d0acc52d8
Rafa G. Miranda ℝ - 𝟙𝟚 <89557826+RafaGdeom@users.noreply.github.com>
2026-03-17 22:30:49 -0300

    Add files via upload

---
+++ 0.07/Program.cs
@@ -0,0 +1,13 @@
+﻿/* 0.07 - Faça um programa que receba o salário de um funcionário e o percentual de aumento, 
+calcule e mostre o valor do aumento e o novo salário.*/
+
+double salario = 0;
+double almento = 0;
+double newSalario = 0;
+
+Console.Write("Digite o valor salario: ");
+salario = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite o percentual do almento: ");
+almento = Convert.ToDouble(Console.ReadLine());
+newSalario = (salario * almento) + salario;
+Console.WriteLine($"O salario reajustado será: R${newSalario}");
